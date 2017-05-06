c6a1c4d746db57e28b33cd2a946aadaf50b837ec
Sherilyn Moline <smoline@gmail.com>
2017-05-06 16:50:34 -0400

    update seed.rb for heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,7 @@
 require 'open-uri'
 require 'nokogiri'
 
-Nokogiri::HTML(open('https://news.ycombinator.com/news?p=1')).css(".storylink").each { |link| Story.create(title: link.content, url: link.values.first, created_by_id: 1, image_data: "{\"original\":{\"id\":\"4.jpg\",\"storage\":\"store\",\"metadata\":{\"filename\":\"img1.jpg\",\"size\":8350801,\"mime_type\":null}},\"large\":{\"id\":\"5.jpg\",\"storage\":\"store\",\"metadata\":{\"filename\":\"down20170413-59947-i2whri.jpg\",\"size\":581387,\"mime_type\":null}},\"medium\":{\"id\":\"6.jpg\",\"storage\":\"store\",\"metadata\":{\"filename\":\"down20170413-59947-i2whri20170413-59947-xzxmma.jpg\",\"size\":248120,\"mime_type\":null}},\"small\":{\"id\":\"7.jpg\",\"storage\":\"store\",\"metadata\":{\"filename\":\"down20170413-59947-i2whri20170413-59947-xzxmma20170413-59947-1yq391i.jpg\",\"size\":97255,\"mime_type\":null}},\"thumbnail\":{\"id\":\"8.jpg\",\"storage\":\"store\",\"metadata\":{\"filename\":\"down20170413-59947-i2whri20170413-59947-xzxmma20170413-59947-1yq391i20170413-59947-1g7uy3y.jpg\",\"size\":6351,\"mime_type\":null}}}") }
+Nokogiri::HTML(open('https://news.ycombinator.com/news?p=1')).css(".storylink").each { |link| Story.create(title: link.content, url: link.values.first, created_by_id: 1 }
 
 # https://news.ycombinator.com
 # https://news.ycombinator.com/news?p=2
